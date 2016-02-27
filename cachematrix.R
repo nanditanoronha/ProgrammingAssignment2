4bf228c9d486ca00f2d4f6d1dc7d69bfe80f50e3
Nandita <nanditanoronha@gmail.com>
2016-02-27 14:18:29 -0500

    R Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is function that has a list of functions that stores your input
+## of singular matrix (in get & set functions) & stores the inverse(in getinverse & setinverse)  
+## While cacheSolve solves and returns the inverse of the input matrix if not previously stored.
 
-## Write a short comment describing this function
+## makeCacheMatrix() takes an input of a matrix and initially sets the variable 'i' as null
+## and uses the set() to define new inputs and reset the 'i' to null. This is later stored in get. 
+## With '<<', we  store the new input as x and resetted 'i' in not just the set function, 
+## but in the entire makeCacheMatrix(). The setinverse() sets the calculated value of the inverse
+## of the matrix which has been calculated and is stored in getinverse(). 
+## Finally, all 4 functions are put in a list.
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -18,7 +24,9 @@ makeCacheMatrix <- function(x = matrix()) {
   
 }
 
-## Write a short comment describing this function
+## cacheSolve() first checks if the inverse of matrix has been stored in 'i'. If it is stored, 
+## it returns the inverse after printing "getting cached data".If not(ie the value of i will be null)
+## and it will find the inverse with the solve function and later set it in the setinverse function.
 
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
